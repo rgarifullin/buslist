88b5fa18c437ddf6e42ee72a2a318d7bb4a2a408
Rinat <ringarifullin@gmail.com>
2016-03-30 00:56:55 +0300

    Add actions to buses controller

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,2 +1,30 @@
 class BusesController < ApplicationController
+  def index
+    @buses = Bus.all
+  end
+
+  def new
+    @bus = Bus.new
+    @bus.build_number_plate
+    @bus.build_vehicle_licence
+  end
+
+  def create
+    bus = Bus.new(bus_params)
+    if bus.save
+      redirect_to buses_path
+    else
+      @bus = bus
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  private
+
+  def bus_params
+    params.require(:bus).permit(:name, number_plate_attributes: [:series, :number, :region], vehicle_licence_attributes: [:digit_code, :letter_code, :number, :release_date])
+  end
 end
