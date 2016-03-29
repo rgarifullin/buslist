4e880a1f81dddb6a847b55e74281173b80d7e331
Rinat <ringarifullin@gmail.com>
2016-03-30 00:57:38 +0300

    Allow nested attributes in Bus model

--- app/models/bus.rb
+++ app/models/bus.rb
@@ -3,4 +3,7 @@ class Bus < ActiveRecord::Base
   has_one :vehicle_licence, dependent: :destroy
 
   validates :name, presence: true
+
+  accepts_nested_attributes_for :number_plate
+  accepts_nested_attributes_for :vehicle_licence
 end
