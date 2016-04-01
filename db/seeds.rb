8ab12f42da92f930d97a0ba4014b0c3066375b43
Rinat <ringarifullin@gmail.com>
2016-04-01 10:10:10 +0300

    Add db seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+number_plate = NumberPlate.new(series: 'ааа', number: 123, region: 116)
+vehicle_licence = VehicleLicence.new(digit_code: 77,
+                                     letter_code: 'то',
+                                     number: 123456,
+                                     release_date: '2016-01-01'
+)
+bus = Bus.create(name: 'Higer',
+                  number_plate: number_plate,
+                  vehicle_licence: vehicle_licence
+)
+
+number_plate2 = NumberPlate.new(series: 'нон', number: 623, region: 116)
+vehicle_licence2 = VehicleLicence.new(digit_code: 78,
+                                      letter_code: 'тт',
+                                      number: 123987,
+                                      release_date: '2016-03-02'
+)
+bus2 = Bus.create(name: 'Paz',
+                  number_plate: number_plate2,
+                  vehicle_licence: vehicle_licence2
+)
